c45e409b8119d0e615bc2ec8aa202d27ca345899
Ben Capp <bentucker333@gmail.com>
2023-03-09 10:52:47 -0600

    removed comment

--- src/redux/sagas/shelf.saga.js
+++ src/redux/sagas/shelf.saga.js
@@ -22,22 +22,6 @@ function* postShelf(action) {
   }
 }
 
-// function* postItem() {
-//     yield takeLatest("POST_ITEM", addItem);
-//   }
-
-//   //WORKER SAGA
-//   function* addItem(action) {
-//     try {
-//       yield axios.post("/api/shelf", action.payload);
-//       // yield put({
-//       //   type: "FETCH_ITEMS",
-//       // });
-//     } catch (error) {
-//       console.log(`error in addItem`, error);
-//     }
-//   }
-
 function* deleteShelf(action) {
   try {
     yield axios.delete(`/api/shelf${action.payload.id}`);
