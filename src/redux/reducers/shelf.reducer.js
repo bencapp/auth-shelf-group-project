b1c507712c632c2cad43f5b9bc281a3cd69d074a
Ben Capp <bentucker333@gmail.com>
2023-03-09 09:12:52 -0600

    created shelf reducer

---
+++ src/redux/reducers/shelf.reducer.js
@@ -0,0 +1,12 @@
+const shelf = (state = [], action) => {
+    switch (action.type) {
+      case 'SET_SHELF':
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+  
+  // user will be on the redux state at:
+  // state.user
+  export default shelf;
