d1b57613138ef619201bcc3052f54ef0acc6f89f
Ben Capp <bentucker333@gmail.com>
2023-03-10 10:06:01 -0600

    put shelflist on login page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import LoginForm from '../LoginForm/LoginForm';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import LoginForm from "../LoginForm/LoginForm";
+import { useHistory } from "react-router-dom";
+
+import ShelfList from "../ShelfPage/ShelfList/ShelfList";
 
 function LoginPage() {
   const history = useHistory();
@@ -8,13 +10,16 @@ function LoginPage() {
   return (
     <div>
       <LoginForm />
+      <h2>Shelf</h2>
+
+      <ShelfList />
 
       <center>
         <button
           type="button"
           className="btn btn_asLink"
           onClick={() => {
-            history.push('/registration');
+            history.push("/registration");
           }}
         >
           Register
