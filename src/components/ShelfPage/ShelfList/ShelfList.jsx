9a41fabbf8b84797229f8239c7182cd262cacd09
Ben Capp <bentucker333@gmail.com>
2023-03-10 09:59:25 -0600

    completed shelf

--- src/components/ShelfPage/ShelfList/ShelfList.jsx
+++ src/components/ShelfPage/ShelfList/ShelfList.jsx
@@ -4,14 +4,11 @@ import { useParams } from "react-router-dom";
 import ShelfItem from "../ShelfItem/ShelfItem";
 import "./ShelfList.css";
 
-function ShelfList() {
+function ShelfList({ myShelf }) {
   const dispatch = useDispatch();
   const shelfList = useSelector((store) => store.shelf);
+
   const user = useSelector((store) => store.user);
-
-  const { id } = useParams();
-
-  console.log(id);
 
   useEffect(() => {
     console.log("in use effect");
@@ -22,10 +19,15 @@ function ShelfList() {
 
   return (
     <section className="shelf-list-style">
-      {id === user.id && <h1>My Shelf</h1>}
-      {shelfList.map((item) => {
-        return <ShelfItem key={item.id} item={item} />;
-      })}
+      {myShelf
+        ? shelfList
+            .filter((item) => item.user_id == user.id)
+            .map((item) => {
+              return <ShelfItem key={item.id} item={item} />;
+            })
+        : shelfList.map((item) => {
+            return <ShelfItem key={item.id} item={item} />;
+          })}
     </section>
   );
 }
--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -2,13 +2,29 @@ import React from "react";
 import ShelfList from "./ShelfList/ShelfList";
 import ShelfForm from "./ShelfForm/ShelfForm";
 
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 function ShelfPage() {
+  const { id } = useParams();
+
+  const user = useSelector((store) => store.user);
+
   return (
     <div className="container">
-      <h2>Shelf</h2>
-      <p>All of the available items can be seen here.</p>
+      {id && Number(id) === Number(user.id) ? (
+        <>
+          <h2>My Shelf</h2> <p>This is the list of items that you added.</p>
+        </>
+      ) : (
+        <>
+          <h2>Shelf</h2>
+          <p>All of the available items can be seen here.</p>
+        </>
+      )}
+
       <ShelfForm />
-      <ShelfList />
+      <ShelfList myShelf={id && Number(id) === Number(user.id)} />
     </div>
   );
 }
